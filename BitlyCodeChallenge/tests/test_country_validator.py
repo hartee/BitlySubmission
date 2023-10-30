20869492986fe2875d828c2e798af353112560a9
ea <erikanthony.harte@gmail.om>
2023-10-30 13:45:59 -0700

    updated country code tests

--- BitlyCodeChallenge/tests/test_country_validator.py
+++ BitlyCodeChallenge/tests/test_country_validator.py
@@ -6,6 +6,8 @@ from bitly.country_validator import CountryValidator
 
 class TestCountryValidator(unittest.TestCase):
     VALID_COUNTRY_EVENT = '{"country_code":"RU"}'
+    ANOTHER_COUNTRY_EVENT = '{"country_code":"RU"}'
+    YET_ANOTHER_COUNTRY_EVENT = '{"country_code":"IN"}'
 
     LOGGER = EventLogger()
     VALIDATOR = CountryValidator()
@@ -15,6 +17,15 @@ class TestCountryValidator(unittest.TestCase):
         self.LOGGER.clear_alerts()
         self.VALIDATOR.validate(self.VALID_COUNTRY_EVENT)
         self.assertEqual(len(self.LOGGER.get_alerts()),0)
+
+    def test_self_report_when_country_code_is_valid_succeeds(self):
+        self.LOGGER.clear_alerts()
+        self.VALIDATOR.validate(self.VALID_COUNTRY_EVENT)
+        self.VALIDATOR.validate(self.ANOTHER_COUNTRY_EVENT)
+        self.VALIDATOR.validate(self.YET_ANOTHER_COUNTRY_EVENT)
+
+        self.assertEqual(self.VALIDATOR.COUNTRY_COUNTS["RU"],2)
+        self.assertEqual(self.VALIDATOR.COUNTRY_COUNTS["IN"],1)
 
 if __name__ == "__main__":
     unittest.main()
